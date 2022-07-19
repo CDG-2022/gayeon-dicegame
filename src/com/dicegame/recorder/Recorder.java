2275f334909cb5f02be4706fa82891afed4c3b26
gayeonP <pagaye@naver.com>
2022-07-19 19:10:01 +0900

    작동하긴 하나 FraudPlayer에 버그가 있음.

--- src/com/dicegame/recorder/Recorder.java
+++ src/com/dicegame/recorder/Recorder.java
@@ -2,6 +2,7 @@ package com.dicegame.recorder;
 
 import com.dicegame.dice.Dice;
 import com.dicegame.dice.FraudDice;
+import com.dicegame.player.FraudPlayer;
 import com.dicegame.player.Player;
 
 import java.util.List;
@@ -10,10 +11,10 @@ public class Recorder {
     public void recordScore(List<Player> players) {
         System.out.printf("[ ");
         for (int i = 0; i < players.size(); i++) {
-            if (i % 2 != 0) getMode(players.get(i)); // i가 홀수일때 핵임
+            if (players.get(i) instanceof FraudPlayer) getMode(players.get(i)); // i가 핵이라면 모드를 가져옴
             System.out.printf(players.get(i).getName() + ":" + players.get(i).getScore() + " ");
         }
-        System.out.printf(" ]\n");
+        System.out.printf("]\n");
     }
 
     private String getMode(Player player) {
@@ -26,6 +27,15 @@ public class Recorder {
             case 2:
                 return "[STRONG] ";
             default: return "";
+        }
+    }
+
+    public void recordWinner(String name) {
+        System.out.println("승자를 기록합니다.");
+        if (name == "") {
+            System.out.println("무승부입니다!");
+        } else {
+            System.out.println(name + "의 승리입니다!");
         }
     }
 }
